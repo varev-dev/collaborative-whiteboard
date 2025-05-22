a96b3e9b48c0966332d6c68086c23f849541ed1a
kox13 <woloszyndawid99@gmail.com>
2025-05-22 18:17:33 +0200

    rewritten program to use async via epoll

--- server/socket-server.c
+++ server/socket-server.c
@@ -1,100 +1,104 @@
-#include "libs/core.h"
-#include "libs/dllist.h"
+#include <sys/socket.h>
+#include <unistd.h>
 
-pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <assert.h>
 
-void* connection_handler(void *conn);
-void  close_connections(DLList **connections);
-void  close_connection(void *connection);
+#include "utils.h"
+#include "dllist.h"
+#include "board.h"
+#include "connection.h"
+#include "server.h"
 
-int main(int argc, char* argv[]) {
-	int listenfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (listenfd == -1) {
-		die("Failed to create a listening socket.\n");
-	}
+int main() {
+	// create server
+	Server *s = server_create(htonl(INADDR_ANY), 5000);
 
-	struct sockaddr_in serv_addr = {};
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	serv_addr.sin_port = htons(5000); 
-
-	if (bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1) {
-		die("Failed to bind the listening socket.\n");
-	}
-
-	if (listen(listenfd, 10) == -1) {
-		die("Failed to listen on the listening socket.\n");
-	}
-
-	Board *board = board_create(10, 10);
-	if (board == NULL) {
-		die("Failed to create a board.\n");
-	}
-
-	DLList *connections = dllist_create();
-	if (connections == NULL) {
-		die("Failed to create connections list.\n");
-	}
-
-    board_print(board);
-
-	int connfd;
-	pthread_t thread_id;
-
-	for (;;) {
-		connfd = accept(listenfd, NULL, NULL);
-		if (connfd == -1) {
-			continue;
-		}
-
-		Connection *connection = connection_create(board, connfd);
-		if (connection == NULL) {
-			error("Failed to create a connection.\n");
-			continue;
-		}
-
-		dllist_push(connections, connection);
-
-		fprintf(stdout, "Connection accepted\n");
-		pthread_create(&thread_id, NULL, connection_handler, connection);
-	}
-
-    board_destroy(&board);
-	dllist_clear(connections, close_connection);
-	dllist_destroy(&connections);
-
-	pthread_mutex_destroy(&lock);
-}
-
-void* connection_handler(void *conn) {
-	Connection *connection = conn;
-	size_t read_size;
-	
-	do {
-		read_size = recv(connection->socket, connection->readbuf, BUFFER_SIZE - 1, 0);
-		if (read_size == 0) {
+	while (true) {
+		// wait for events
+		int count = epoll_wait(s->epoll_fd, s->events, s->max_events, -1);
+		if (count == -1) {
+			error("Waiting for events: %s\n", ERRNO_STR);
 			break;
 		}
 
-		connection->readbuf[read_size] = '\0';
+		for (int i = 0; i < count; i++) {
+			if (s->events[i].data.ptr == s->event_ptr) {
+				// if accept
+				int conn_fd = accept(s->fd, NULL, NULL);
+				if (conn_fd == -1) {
+					error("Accepting a connection: %s\n", ERRNO_STR);
+					continue;
+				}
 
-		for (int i = 0; i < read_size; i++) {
-			if (connection->readbuf[i] >= 'a' && connection->readbuf[i] <= 'z')
-			connection->readbuf[i] -= 32;
+				// create connection
+				Connection *c = connection_create(conn_fd);
+				if (c == NULL) {
+					error("Creating a connection: %s\n", ERRNO_STR);
+					close(conn_fd);
+					continue;
+				}
+
+				// add connection to server
+				if (server_add_connection(s, c) == -1) {
+					connection_close(&c);
+				}
+
+				if (c != NULL) {
+					logg("Connection %i accepted.\n", c->id);
+				}
+
+				continue;
+			}
+
+			if (s->events[i].events & READ) {
+				// if read
+				assert(s->events[i].data.ptr != NULL);
+
+				Connection *c = s->events[i].data.ptr;
+
+				// read from connection
+				size_t size = read(c->fd, c->readbuf, BUFFER_SIZE - 1);
+				if (size == -1) {
+					error("Reading from socket: %s\n", ERRNO_STR);
+					continue;
+				}
+
+				c->readbuf[size] = '\0';
+				// decode message
+				// (create session, join session, update board, get board)
+
+				memcpy(c->writebuf, c->readbuf, size + 1);
+				for (int i = 0; i < size; i++) {
+					if (c->writebuf[i] >= 'a' && c->writebuf[i] <= 'z') {
+						c->writebuf[i] -= 32;
+					}
+				}
+
+				server_set_connection_mode(s, c, READ | WRITE);
+			}
+
+			if (s->events[i].events & WRITE) {
+				// if write
+				assert(s->events[i].data.ptr != NULL);
+
+				Connection *c = s->events[i].data.ptr;
+
+				// write to connection
+				size_t size = write(c->fd, c->writebuf, strlen(c->writebuf));
+				if (size == -1) {
+					error("Writing to socket: %s\n", ERRNO_STR);
+					continue;
+				}
+
+				server_set_connection_mode(s, c, READ);
+			}
+			// if close
 		}
+	}
 
-		// handle_request(connection->readbuf);
-		write(connection->socket, connection->readbuf, read_size);
-
-		memset(connection->readbuf, 0, BUFFER_SIZE);
-	} while(read_size > 2);
-	
-	fprintf(stderr, "Client disconnected\n");
-	
-	connection_close(&connection);
-	pthread_exit(NULL);
-}
-
-void close_connection(void *connection) {
-	connection_close((Connection**)&connection);
+	server_destroy(&s);
 }
